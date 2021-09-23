ee7b742e71d7dac8ecd4f6c13537075f83e99641
namknf <86828067+namknf@users.noreply.github.com>
2021-09-23 12:45:10 +0300

    init

---
+++ binarySearch.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace algoritnms
+{
+    class Program
+    {
+        static void Main()
+        {
+           binary_search();  
+        }
+    }
+
+    class Alg
+    {
+        void binary_search()
+        {
+           
+        }
+    }
+}
