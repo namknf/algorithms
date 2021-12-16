b212580b6b3ed75a4d50575ab24e8e5d95a4898c
Anastasia <86828067+namknf@users.noreply.github.com>
2021-12-16 08:57:23 +0300

    updated BinarySearch

--- binarySearch.cs
+++ binarySearch.cs
@@ -1,29 +1,20 @@
-using System;
-using Collections.Generic;
-
-namespace algorithms
+namespace Algorithms
 {
-    class Program
-    {
-        static void Main()
-        {
-            BinarySearch();
-        }
-    }
-
-    class Alg()
+    using System;
+    
+    internal class Alg()
     {
         public void BinarySearch(int[] array, int searchVar, int low, int high)
         {
-           while(low <= high)
+           while (low <= high)
            {
-               var mid = (low + high)/2;
+               var mid = (low + high) / 2;
 
-               if(searchVar == mid)
+               if (searchVar == mid)
                {
                    return mid;
                }
-               else if(searchVar < mid)
+               else if (searchVar < mid)
                {
                    high = mid - 1;
                }
@@ -32,7 +23,22 @@ namespace algorithms
                    low = mid + 1;
                }
            }
+            
            return -1;
+        }
+    }
+    
+    class Program
+    {
+        static void Main()
+        {
+            Alg test = new Alg();
+            
+            int[] array = new int[] {2, 4, 6, 8, 10, 12};
+            
+            int searchRes = 6;
+   
+            test.BinarySearch(array, searchRes, 0, array.Length - 1);
         }
     }
 }
