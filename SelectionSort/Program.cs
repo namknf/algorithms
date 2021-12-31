d1d6a3c08c6a456e7e18a3840de4151feb5e237b
anastasiya_malkina <9093264418es@gmail.com>
2021-12-31 17:40:38 +0300

    SelectionSort is completed

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace SelectionSort
+{
+    using System;
+
+    class Program
+    {
+        static int findSmallest(int[] array, int n)
+        {
+            int result = n;
+
+            for (var i = n; i < array.Length; ++i)
+            {
+                if (array[i] < array[result])
+                {
+                    result = i;
+                }
+            }
+
+            return result;
+        }
+
+        static void Swap(ref int a, ref int b)
+        {
+            var c = a;
+            a = b;
+            b = c;
+        }
+
+        static int[] SelSort(int[] array, int currentIndex = 0)
+        {
+            if (currentIndex == array.Length)
+            {
+                return array;
+            }
+
+            var index = findSmallest(array, currentIndex);
+
+            if (index != currentIndex)
+            {
+                Swap(ref array[index], ref array[currentIndex]);
+            }
+
+            return SelSort(array, currentIndex + 1);
+        }
+
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 3, 4, 6, 8, 10, 2, 0, 1, 1 };
+            Console.WriteLine(string.Join(", ", SelSort(array)));
+        }
+    }
+}
