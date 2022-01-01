a811ed7a7c7074d0865d0c31b2b020818c2497e6
anastasiya_malkina <9093264418es@gmail.com>
2022-01-01 12:24:07 +0300

    added methods Combinatios and Factorial

---
+++ Recursion.cs
@@ -0,0 +1,36 @@
+namespace Algorithms
+{
+    using System;
+
+    internal class AlgFactroial
+    {
+        public int Factorial(int x)
+        {
+            if (x == 1)
+            {
+                return 1;
+            }
+
+            return x * Factorial(x - 1);
+        }
+
+        public int Combinations(int m, int n)
+        {
+            int result = 0;
+
+            if ((m == 0) && (n > 0) || (m == n) && (n > 0))
+            {
+                result = 1;
+            }
+            else if ((m > n) && (n >= 0))
+            {
+                result = 0;
+            }
+            else
+            {
+                result = Combinations(n - 1, m - 1) + Combinations(n - 1, m);
+            }
+            return result;
+        }
+    }
+}
